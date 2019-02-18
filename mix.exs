4efcf5e1c676313540c94754ebd23f78b7846dd4
Nathan Sutton <nate@zencoder.com>
2019-02-18 15:39:31 -0600

    Get some packaging stuff setup

--- mix.exs
+++ mix.exs
@@ -5,24 +5,29 @@ defmodule AppEnv.MixProject do
     [
       app: :app_env,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.6",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
+    [{:ex_doc, "~> 0.19", only: :dev}]
+  end
+
+  defp package do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      maintainers: ["Nathan Sutton"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/nate/app_env"}
     ]
   end
 end
